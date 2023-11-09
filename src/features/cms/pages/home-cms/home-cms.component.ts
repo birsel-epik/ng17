de57b8c4bb66ab6983645b347fd4f72ef2a4f294
BirselE <birsel.epik@gmail.com>
2023-11-09 21:58:46 +0300

    Deferrable views

--- src/features/cms/pages/home-cms/home-cms.component.ts
+++ src/features/cms/pages/home-cms/home-cms.component.ts
@@ -1,18 +1,19 @@
-import { Component, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DeferComponent } from "../../components/defer/defer.component";
-import { Defer3Component } from "../../components/defer-3/defer-3.component";
-import { Defer2Component } from "../../components/defer-2/defer-2.component";
+import {Component, signal} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {DeferComponent} from "../../components/defer/defer.component";
+import {Defer3Component} from "../../components/defer-3/defer-3.component";
+import {Defer2Component} from "../../components/defer-2/defer-2.component";
+import {first} from "rxjs";
 
 @Component({
-    selector: 'app-home-cms',
-    standalone: true,
-    templateUrl: './home-cms.component.html',
-    styleUrls: ['./home-cms.component.scss'],
-    imports: [CommonModule, DeferComponent, Defer3Component, Defer2Component]
+  selector: 'app-home-cms',
+  standalone: true,
+  templateUrl: './home-cms.component.html',
+  styleUrls: ['./home-cms.component.scss'],
+  imports: [CommonModule, DeferComponent, Defer3Component, Defer2Component]
 })
 export class HomeCmsComponent {
-  sinyal = signal(false)
+  signal = signal(false)
   isChecked = signal(true)
 
   num = signal(58)
@@ -20,14 +21,17 @@ export class HomeCmsComponent {
   isCheckedDefer = signal(false);
 
   collection = [
-    { id: 1, name: 'Item 1' },
-    { id: 2, name: 'Item 2' },
-    { id: 3, name: 'Item 3' }
+    {id: 1, name: 'Item 1'},
+    {id: 2, name: 'Item 2'},
+    {id: 3, name: 'Item 3'}
   ];
 
   emptyCollection: any = [];
 
   radioValue = signal(0);
-  
+
+  protected readonly first = first;
+  even: string | undefined;
+  odd: string | undefined;
 }
 
